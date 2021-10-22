fe0022929f5d9b9b4d7d532ac09f4ec531c416fd
chentaiwu <chentaiwu@zonst.cn>
2021-10-22 16:52:14 +0800

    mocking test

---
+++ mocking/v5/countdown_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationsSpy{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got '%s', but want '%s'", got, want)
+		}
+
+	})
+
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySleeperPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleeperPrinter, spySleeperPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleeperPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleeperPrinter.Calls)
+		}
+	})
+}
